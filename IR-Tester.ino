6ddf1839f8274df526a4d57ae8c6d8f8d5074166
alexlynd <alexlynd@protonmail.com>
2018-12-28 13:18:19 -0800

    Arduino file

---
+++ IR-Tester.ino
@@ -0,0 +1,43 @@
+#include <IRremote.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+const int RECV_PIN= 7;  // IR data pin
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.clearDisplay();
+  display.println("IR Reader by Alex");
+  display.println("V 1.0");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.display();
+}
+
+void loop(){
+  if (irrecv.decode(&results)){
+        display.setTextSize(1);
+        display.setTextColor(WHITE);
+        display.setCursor(0,0);
+        display.clearDisplay();
+        display.print("Hex:");
+        display.println(results.value, HEX);
+        display.display();
+        irrecv.resume();
+  }
+}
